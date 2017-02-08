cb1445a7cee9f2cb4c53deed7c9eab300ce10667
Jake Lawley <jakelawley@hotmail.co.uk>
2017-02-08 13:44:09 +0000

    Update main.py

--- JakeGuardianApp.tingapp/main.py
+++ JakeGuardianApp.tingapp/main.py
@@ -1,35 +1,59 @@
 import tingbot
 from tingbot import *
 
+
+import time
 import os
 import requests, json
 from HTMLParser import HTMLParser
 
+######################## GLOBALS ########################
+state = {}
+jsonResp = ''
+webTitle = ''
+webPublicationDate = ''
+webUrl = ''
+index = 0
+######################## GLOBALS ########################
+
 # make call
+@every(minutes=5)
+def get_data():
 
-@every(minutes=10)
-def refresh_data():
-
-    req = requests.get('http://content.guardianapis.com/search?q=technology&api-key=9570cf30-7bc8-4683-8069-0cea7b060980')
+    req = requests.get('http://content.guardianapis.com/search?q=uk&api-key=9570cf30-7bc8-4683-8069-0cea7b060980')
     
-    resp = req.json()
+    global jsonResp
+    jsonResp = req.json()
     
-    headline1 = HTMLParser().unescape(resp['response']['results'][0]['sectionName'])
+    refresh_feed()
+    
+#parse content
+def refresh_feed():
+    global webTitle
+    webTitle = HTMLParser().unescape(jsonResp['response']['results'][index]['webTitle'])
 
-    screen.text(headline1, color='blue', font_size=16)    
 
+######################## BUTTONS ########################
+@right_button.press
+@left_button.press
+def on_right():
+    global index
+    index = index +1
+    refresh_feed()
+    
+@left_button.press
+def on_left():
+    global index
+    index = index -2
+    refresh_feed()
 
-
-
-
-
+######################## BUTTONS ########################
 
 
 @every(seconds=1.0/30)
 def loop():
-    # drawing code here
-    screen.fill(color='black')
-    
-    refresh_data()
+    # SETUP UI
+    screen.fill(color='blue')
+    screen.text(webTitle, color='white', font_size=16)
 
 tingbot.run()
